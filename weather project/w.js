1cf829b0e060f19e6f766ddbc70288fb4a7ff49a
Swarnava-paul <swarnavapaul108@gmail.com>
2023-12-23 23:03:52 +0530

    weather project

---
+++ weather project/w.js
@@ -0,0 +1,23 @@
+/*if (navigator.geolocation) {
+    // Geolocation is supported
+    navigator.geolocation.getCurrentPosition(successCallback);
+  } else {
+    // Geolocation is not supported
+    console.log("Geolocation is not supported by your browser");
+  }
+  
+  function successCallback(position) {
+    // Get the latitude and longitude from the position object
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    
+  
+    console.log("Latitude: " + latitude);
+    console.log("Longitude: " + longitude);
+  }*/
+
+  //location 
+
+
+
+  
